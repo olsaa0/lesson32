00d0249bd49b5ca59e5faa5bea3027d14c09f527
olsaa0 <olsaolsa30@gmail.com>
2024-11-05 16:05:46 +0100

    loopa

---
+++ main.js
@@ -0,0 +1,13 @@
+
+var lista=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+ var shuma = 0;
+for(i=0; i<15; i++){
+    shuma += lista[i];
+}
+ console.log(shuma)
+
+ var shuma2=0;
+ for(x of numrat){
+    shuma2+=x;
+ }
+ console.log(shuma2);
